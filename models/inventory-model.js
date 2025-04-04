460188a2cb72fdaa0ead059404199ca6b1ccf592
olubisiayantoye <bisikingson@gmail.com>
2025-04-04 10:19:32 +0100

    new file:   .node_version 	new file:   controllers/accountController.js 	new file:   controllers/baseController.js 	new file:   controllers/invController.js 	new file:   controllers/messageController.js 	new file:   database/Week-6-Messages.sql 	new file:   database/assignment2.sql 	new file:   database/bisi_db-sql-configure.sql 	new file:   database/db-sql-configure.sql 	new file:   database/index.js 	new file:   database/inventory-data.sql 	new file:   models/account-model.js 	new file:   models/inventory-model.js 	new file:   models/message-model.js 	new file:   npm 	modified:   package.json 	deleted:    personal-finance-tracker/package.json 	modified:   pnpm-lock.yaml 	new file:   public/css/base.css 	new file:   public/css/large.css 	new file:   public/css/medium.css 	new file:   public/css/normalize.css 	deleted:    public/css/styles.css 	new file:   public/images/vehicles/bat-tn.jpg 	new file:   public/images/vehicles/bat.jpg 	new file:   public/images/vehicles/crown-vic-tn.jpg 	new file:   public/images/vehicles/crown-vic.jpg 	new file:   public/images/vehicles/dog-tn.jpg 	new file:   public/images/vehicles/dog.jpg 	new file:   public/images/vehicles/fbi-tn.jpg 	new file:   public/images/vehicles/fbi.jpg 	new file:   public/images/vehicles/ford-modelt-tn.jpg 	new file:   public/images/vehicles/ford-modelt.jpg 	new file:   public/images/vehicles/jeep-wrangler-tn.jpg 	new file:   public/images/vehicles/jeep-wrangler.jpg 	new file:   public/images/vehicles/lambo-Adve-tn.jpg 	new file:   public/images/vehicles/lambo-Adve.jpg 	new file:   public/images/vehicles/mm-tn.jpg 	new file:   public/images/vehicles/mm.jpg 	new file:   public/images/vehicles/monster-tn.jpg 	new file:   public/images/vehicles/monster.jpg 	new file:   public/images/vehicles/ms-tn.jpg 	new file:   public/images/vehicles/ms.jpg 	new file:   public/js/inventory.js 	new file:   routes/accountRoute.js 	new file:   routes/inventoryRoute.js 	new file:   routes/messageRoute.js 	modified:   server.js 	new file:   utilities/account-validation.js 	new file:   utilities/auth.js 	new file:   utilities/index.js 	new file:   utilities/inventory-validation.js 	new file:   utilities/message-validation.js 	new file:   views/account/login.ejs 	new file:   views/account/management.ejs 	new file:   views/account/register.ejs 	new file:   views/account/update.ejs 	new file:   views/errors/error.ejs	modified:   views/index.ejs 	new file:   views/inventory/add-classification.ejs 	new file:   views/inventory/add-inventory.ejs 	new file:   views/inventory/classification.ejs 	new file:   views/inventory/delete-confirm.ejs 	new file:   views/inventory/details.ejs 	new file:   views/inventory/edit-inventory.ejs 	new file:   views/inventory/management.ejs 	modified:   views/layouts/layout.ejs 	new file:   views/messages/archived.ejs 	new file:   views/messages/display.ejs 	new file:   views/messages/inbox.ejs 	new file:   views/messages/new.ejs 	new file:   views/messages/reply.ejs 	new file:   views/messages/sent.ejs 	modified:   views/partials/footer.ejs 	modified:   views/partials/head.ejs 	modified:   views/partials/header.ejs 	modified:   views/partials/navigation.ejs

---
+++ models/inventory-model.js
@@ -0,0 +1,115 @@
+const pool = require("../database/")
+
+
+/* ***************************
+ *  Get all classification data
+ * ************************** */
+async function getClassifications(){
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+}
+
+/* *****************************
+*   Register new Classification
+* *************************** */
+async function registerClassification(classification_name){
+  try {
+    const sql = "INSERT INTO classification (classification_name) VALUES ($1) RETURNING *"
+    return await pool.query(sql, [classification_name])
+  } catch (error) {
+    return error.message
+  }
+}
+
+async function addInventory(inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id){
+  try {
+    const sql = "INSERT INTO inventory (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *"
+    return await pool.query(sql, [inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id])
+  } catch (error) {
+    return error.message
+  }
+}
+
+async function updateInventory(inv_id, inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id){
+  try {
+    const sql = "UPDATE inventory SET inv_make=$1, inv_model=$2, inv_year=$3, inv_description=$4, inv_image=$5, inv_thumbnail=$6, inv_price=$7, inv_miles=$8, inv_color=$9, classification_id=$10 WHERE inv_id=$11  RETURNING *"
+    const data = await pool.query(sql, [inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id, inv_id])
+    return data.rows[0]
+  } catch (error) {
+    console.error(`model error: ${error}`)
+  }
+}
+
+async function deleteVehicle(inv_id) {
+  try {
+    const sql = "DELETE from inventory WHERE inv_id=$1 RETURNING *"
+    const data = await pool.query(sql, [inv_id])
+    return data.rows[0]
+  } catch (error) {
+    return error.message
+  }
+}
+
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`,
+      [classification_id]
+    )
+    return data.rows
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
+  }
+}
+
+async function getVehicleById(vehicle_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory
+       WHERE inv_id = $1`,
+       [vehicle_id]
+    )
+    //console.log(data.rows)
+    return data.rows
+
+  } catch (error) {
+    console.error(`getVehicleById error ${error}`)
+  }
+}
+
+async function checkExistingCat(classification_name){
+  try {
+    const sql = "SELECT * FROM classification WHERE classification_name = $1"
+    const classification = await pool.query(sql, [classification_name])
+    return  classification.rowCount
+  } catch (error) {
+    return error.message
+  }
+}
+
+async function checkExistingCatById(classification_id){
+  try {
+    const sql = "SELECT * FROM classification WHERE classification_id = $1"
+    const classification = await pool.query(sql, [classification_id])
+    return  classification.rowCount
+  } catch (error) {
+    return error.message
+  }
+}
+
+module.exports = {
+    getClassifications,
+    getInventoryByClassificationId,
+    getVehicleById,
+    registerClassification,
+    addInventory,
+    updateInventory,
+    deleteVehicle,
+    checkExistingCat,
+    checkExistingCatById
+}
--- server.js
+++ server.js
@@ -7,24 +7,88 @@
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
+const session = require("express-session")
+const pool = require('./database/')
+const account = require("./routes/accountRoute")
+const messages = require("./routes/messageRoute")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 /* ***********************
- * View Engine and Templates
+ * Middleware
+ * ************************/
+
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(require('connect-flash')())
+app.use((req,res,next)=>{
+  res.locals.messages = require('express-messages')(req,res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser())
+app.use(utilities.Util.checkJWTToken)
+
+/* ***********************
+ * View Engine Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
-
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" }); // No 'partials/' prefix needed
-});
+
+//Index route
+app.get("/", utilities.Util.handleErrors(baseController.buildHome))
+
+app.get("/error", utilities.Util.handleErrors(baseController.error))
+
+// Inventory routes
+app.use("/inv", utilities.Util.handleErrors(inventoryRoute))
+
+// Account routes
+app.use("/account", utilities.Util.handleErrors(account))
+
+app.use("/messages", utilities.Util.handleErrors(messages))
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.Util.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
